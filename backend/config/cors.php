c82e0a325cb32840bb2739bb466f51fbe5ab3991
VikramKumar-1 <vikuraj209@gmail.com>
2026-03-04 23:05:24 +0530

    Fix CORS for Vercel frontend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,9 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://chat-app-beryl-three-75.vercel.app',
+   /* 'allowed_origins' => ['https://chat-app-beryl-three-75.vercel.app',
         'http://localhost:5173'
-    ],
+    ],*/
+    
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
